3058bf2e3623e7c643d336100373bea9b8eb34ef
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-24 00:20:31 -0300

    first configuration backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,1 +1,12 @@
-require('./config/server')
+const port = 3003;
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(port, function () {
+  console.log(`backend is running on port ${port}`);
+});
