3e78f5c3ba67f196c5346529b72230c7b32379f3
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-25 00:37:28 -0300

    routes and insominia configuration

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,4 +3,8 @@ const express = require('express');
 module.exports = function (server) {
     const router = express.Router();
     server.use('/api', router);
+    
+    const todoService = require('../api/todo/todoService')
+    //* usa todos os métodos [get, post, put, delete]
+    todoService.register(router, '/todos')
 };
