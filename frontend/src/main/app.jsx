3d746f4c361b315493110a7ac87b7e7a60c741f9
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-30 23:36:04 -0300

    created components routes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,15 +1,13 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
-import Todo from '../todo/todo';
-import About from '../about/about';
 import React from 'react';
 import Menu from '../template/menu';
+import Routes from './Routes';
 
 export default (props) => (
     <div className="container">
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 );
