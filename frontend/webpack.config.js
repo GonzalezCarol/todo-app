887a2fd236410c36d1f4e634b7012dcdcfad584e
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-03-30 23:20:45 -0300

    created component Menu

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,11 @@
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.js'
+        filename: './app.js',
     },
     devServer: {
         port: 8080,
@@ -14,27 +14,29 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + '/node_modules'
-        }
+            modules: __dirname + '/node_modules',
+        },
     },
-    plugins: [ 
-        new ExtractTextPlugin('app.css')
-    ],
+    plugins: [new ExtractTextPlugin('app.css')],
     module: {
-        loaders: [{
-            test: /.js[x]?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
-            }
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader: 'file'
-        }]
-    }
-}
+        loaders: [
+            {
+                test: /.js[x]?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-object-rest-spread'],
+                },
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+            },
+            {
+                test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+                loader: 'file',
+            },
+        ],
+    },
+};
