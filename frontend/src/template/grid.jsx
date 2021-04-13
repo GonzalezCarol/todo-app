ef45e163d92acb5cce952c6ea2f5fef9bd98edf1
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-04-12 23:07:10 -0300

    Todo components if component created

--- frontend/src/template/grid.jsx
+++ frontend/src/template/grid.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
-export const grid = (numbers) => {
-    const cols = number ? numbers.split('') : [];
-    let classes = '';
+export default class Grid extends Component {
+    toCssClasses(numbers) {
+        const cols = numbers ? numbers.split('') : [];
+        let classes = '';
 
-    if (cols[0]) classes += `cols-xs-${cols[0]}`;
-    if (cols[1]) classes += `cols-sm-${cols[1]}`;
-    if (cols[2]) classes += `cols-md-${cols[2]}`;
-    if (cols[3]) classes += `cols-lg-${cols[3]}`;
+        if (cols[0]) classes += `cols-xs-${cols[0]}`;
+        if (cols[1]) classes += `cols-sm-${cols[1]}`;
+        if (cols[2]) classes += `cols-md-${cols[2]}`;
+        if (cols[3]) classes += `cols-lg-${cols[3]}`;
 
-    return classes;
-};
+        return classes;
+    }
+    render() {
+        const gridClasses = this.toCssClasses(this.props.cols || 12);
+        return <div className={gridClasses}>{this.props.children}</div>;
+    }
+}
