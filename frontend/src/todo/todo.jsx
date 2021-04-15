e7451ee18993b0e6a625ade5448cb97ae7071bf2
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-04-14 23:40:36 -0300

    Todo components add new event with axios put

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
+
+const URL = 'http://localhost:3003/api/todos';
 export default class Todo extends Component {
     constructor(props) {
         super(props);
@@ -11,7 +15,10 @@ export default class Todo extends Component {
         this.handleAdd = this.handleAdd.bind(this);
     }
     handleAdd() {
-        console.log(this);
+        const description = this.state.description;
+        axios
+            .post(URL, { description })
+            .then((resp) => console.log('Funcionou'));
     }
 
     handleChange(e) {
