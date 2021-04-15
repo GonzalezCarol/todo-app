74af91744b2c74ced655bd883c239c48cdd579d7
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-04-15 00:25:39 -0300

    Deleting todo list rows

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,3 +1,32 @@
 import React from 'React';
+import IconButton from '../template/iconButton';
 
-export default (props) => <h1>List</h1>;
+export default (props) => {
+    const renderRows = () => {
+        const list = props.list || [];
+        return list.map((todo) => (
+            <tr id={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton
+                        style="danger"
+                        icon="trash-o"
+                        onClick={() => props.handleRemove(todo)}
+                    ></IconButton>
+                </td>
+            </tr>
+        ));
+    };
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>{renderRows()}</tbody>
+        </table>
+    );
+};
